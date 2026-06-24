68c918f784a448c2ee3d7298f1e2bb4c1dc1895d
Alemanuel <alemanuelj5@gmail.com>
2026-06-24 12:21:38 -0600

    validacion automática

--- src/index.js
+++ src/index.js
@@ -2,13 +2,29 @@ require('dotenv').config();
 const express = require('express');
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
+const OpenApiValidator = require('express-openapi-validator');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const swaggerDocument = YAML.load('./openapi.yaml')
-
+const swaggerDocument = YAML.load('openapi.yaml');
 app.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+
+app.use(
+    OpenApiValidator.middleware({
+        apiSpec: swaggerDocument,
+        validateRequests: true,
+        validateResponses: true,
+        ignorePaths: /.*\/docs$/,
+    })
+);
+
+app.use((err, req, res, next) => {
+    res.status(err.status || 500).json({
+        message: err.message,
+        errors: err.errors,
+    });
+});
 
 app.get('/hello', (req, res) => {
     res.json({ message: 'Hello World!' });
